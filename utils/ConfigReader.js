a2c6ac289597df75bf0fd79bdc158a7b1584b2f6
adsingh-akashdeep <adsingh.akashdeep@gmail.com>
2025-12-18 13:11:48 -0500

    Update ConfigReader.js

--- utils/ConfigReader.js
+++ utils/ConfigReader.js
@@ -15,8 +15,3 @@ export class ConfigReader{
 
 }
 
-/*
-| Term   | ------------- Meaning ---------| ------------ Example -----------------|
-| `.env` | File that stores configuration | `BASE_URL=https://jmird.testrail.io/` |
-| `dotenv` | Library that reads `.env` and injects values into`process.env` | `dotenv.config()` |
-*/ 
